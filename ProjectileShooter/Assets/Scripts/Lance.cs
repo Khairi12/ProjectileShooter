aeb3d992adbe119c3a3bee0e8785b5b5a1fbb556
BuildTools <unconfigured@null.spigotmc.org>
2018-08-19 21:20:38 -0500

    Power Bar added
    right click the mouse to charge up

---
+++ ProjectileShooter/Assets/Scripts/Lance.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Lance : MonoBehaviour
+{
+    private GameObject powerBar;
+    private Slider powerBarSlider;
+
+	private void Start ()
+    {
+        powerBar = GameObject.FindGameObjectWithTag("PowerBar");
+        powerBarSlider = powerBar.GetComponent<Slider>();
+	}
+	
+	private void Update ()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            powerBarSlider.value = 0;
+            powerBar.SetActive(true);
+            transform.localRotation = Quaternion.Euler(new Vector3(0, 0, 90));
+        }
+        else if (Input.GetMouseButton(1))
+        {
+            powerBarSlider.value += 1 * Time.deltaTime;
+        }
+        else if (Input.GetMouseButtonUp(1))
+        {
+            powerBarSlider.value = 0;
+            powerBar.SetActive(false);
+            transform.localRotation = Quaternion.Euler(new Vector3(0, 0, 0));
+        }
+	}
+}
